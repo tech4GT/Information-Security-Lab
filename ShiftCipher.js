7bfbcb50728a8c3c4f7e0b83ab918c9ba7b380a2
tech4GT <varun.gupta1798@gmail.com>
2018-09-25 13:16:54 +0530

    done with Z26
    Signed-off-by: tech4GT <varun.gupta1798@gmail.com>

--- ShiftCipher.js
+++ ShiftCipher.js
@@ -1,8 +1,10 @@
+/* Shift cipher on Z26 */
+
 const SHIFT_KEY = 3;
 
 
 (function() {
-    let str = "Secret string"
+    let str = "secret string"
     let enc = encrypt(str);
     let dec = decrypt(enc);
     console.log(str);
@@ -12,10 +14,13 @@ const SHIFT_KEY = 3;
 
 
 function encrypt(input) {
+    input = input.split(" ").join('');
     let rv = "";
     for (ch in input) {
         let charCode = input.charCodeAt(ch);
-        charCode = charCode + SHIFT_KEY;
+        charCode -= 97;
+        charCode = (charCode + SHIFT_KEY) % 26;
+        charCode += 65;
         rv += String.fromCharCode(charCode);
     }
     return rv;
@@ -24,7 +29,9 @@ function decrypt(input) {
     let rv = "";
     for (ch in input) {
         let charCode = input.charCodeAt(ch);
-        charCode = charCode - SHIFT_KEY;
+        charCode -= 65;
+        charCode = (charCode - SHIFT_KEY + 26) % 26;
+        charCode += 97;
         rv += String.fromCharCode(charCode);
     }
     return rv;
